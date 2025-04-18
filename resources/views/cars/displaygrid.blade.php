c4d7fd980157ec8652e73cd51bfb15c3e01a3ad4
Kelvinu293 <b00141682@mytudublin.ie>
2025-04-18 01:24:08 +0100

    adding a navbar to checkout and delete

--- resources/views/cars/displaygrid.blade.php
+++ resources/views/cars/displaygrid.blade.php
@@ -1,12 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<div style="padding-top:1%">
+    <nav class="navbar navbar-right navbar-expand-sm navbar-dark bg-dark">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><button id="checkOut" onclick="window.location.href=''" type="button" style="margin-right:5px;" class="btn btn-primary navbar-btn center-block">Check Out</button></li>
+            <li class="nav-item"><button id="emptycart" type="button" style="margin-right:5px;" class="btn btn-primary navbar-btn center-block">Empty Basket</button></li>
+            <li class="nav-item"><span style="font-size:40px;margin-right:0px;" class="glyphicon glyphicon-shopping-cart navbar-btn"></span></li>
+            <li class="nav-item"><div class="navbar-text" id="shoppingcart" style="font-size:12pt;margin-left:5px;margin-right:0px;"></div></li>
+            <li class="nav-item"><div class="navbar-text" style="font-size:14pt;margin-left:0px;">Select Car</div></li>
+        <ul>
+    </nav>
+</div>
+
 <div class='container'>
     <h2 class="text-center my-4">Car showroom</h2>
 
     <div class='d-flex flex-wrap align-content-start bg-light'>
 
-        {{-- Audi --}}
+
         <div class="p-2 border col-4 g-3">
             <div class="card text-center">
                 <div class="card-header"><h5>Audi A1 Hatchback</h5></div>
@@ -14,7 +26,7 @@
                     <img style="width:65%; height:200px;" class="mx-auto d-block" src="{{ asset('img/audi.jpg') }}" alt="Audi A1">
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-success mx-auto d-block">Add to Cart</button>
+                    <button class="btn btn-success mx-auto d-block">Add to Basket</button>
                 </div>
             </div>
         </div>
@@ -26,7 +38,7 @@
                     <img style="width:65%; height:200px;" class="mx-auto d-block" src="{{ asset('img/bmw.jpg') }}" alt="BMW X5">
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-success mx-auto d-block">Add to Cart</button>
+                    <button class="btn btn-success mx-auto d-block">Add to Basket</button>
                 </div>
             </div>
         </div>
@@ -38,7 +50,7 @@
                     <img style="width:65%; height:200px;" class="mx-auto d-block" src="{{ asset('img/honda.jpg') }}" alt="Honda Civic">
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-success mx-auto d-block">Add to Cart</button>
+                    <button class="btn btn-success mx-auto d-block">Add to Basket</button>
                 </div>
             </div>
         </div>
@@ -50,7 +62,7 @@
                     <img style="width:65%; height:200px;" class="mx-auto d-block" src="{{ asset('img/hyundai.jpg') }}" alt="Hyundai i20">
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-success mx-auto d-block">Add to Cart</button>
+                    <button class="btn btn-success mx-auto d-block">Add to Basket</button>
                 </div>
             </div>
         </div>
@@ -62,7 +74,7 @@
                     <img style="width:65%; height:200px;" class="mx-auto d-block" src="{{ asset('img/toyota.jpg') }}" alt="Toyota Aygo X">
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-success mx-auto d-block">Add to Cart</button>
+                    <button class="btn btn-success mx-auto d-block">Add to Basket</button>
                 </div>
             </div>
         </div>
@@ -74,7 +86,7 @@
                     <img style="width:65%; height:200px;" class="mx-auto d-block" src="{{ asset('img/volkswagen.jpg') }}" alt="VW Golf">
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-success mx-auto d-block">Add to Cart</button>
+                    <button class="btn btn-success mx-auto d-block">Add to Basket</button>
                 </div>
             </div>
         </div>
